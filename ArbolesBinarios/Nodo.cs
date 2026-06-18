ede58b0c6a5fec381357ac30b9a7a83d084d5177
Jahel Corona Moreno <jcorona.0812@gmail.com>
2026-06-17 22:02:28 -0600

    Clase 5: Implementacion de arboles binarios y busqueda

---
+++ ArbolesBinarios/Nodo.cs
@@ -0,0 +1,12 @@
+public class Nodo
+{
+    public int ID {get; set;}
+    public string Dato {get;set;} = string.Empty;
+    public Nodo? HijoIzquierdo {get; set;}
+    public Nodo? HijoDerecho {get; set;}
+    public Nodo(int id, string dato)
+    {
+        ID =id;
+        Dato = dato;
+    }
+}
