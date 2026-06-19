fe358d3f327a1efe9d5bc397b4ff6dd12c6d7135
Jahel Corona Moreno <jcorona.0812@gmail.com>
2026-06-18 20:44:05 -0600

    Clase 7: Simulacion del Call Stack y Recursividad

---
+++ RecursividadTeoria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== EJERCICIO A: SIMULACIÓN DEL CALL STACK ===");
+
+        SimuladorStack.ImprimirCuentaRegresiva(5);
+
+        Console.WriteLine("\n=== EJERCICIO B: SUMA RECURSIVA ===");
+
+        int numero = Validador.LeerNumero();
+
+        int resultado = Validador.SumarHasta(numero);
+
+        Console.WriteLine($"La suma de 1 hasta {numero} es: {resultado}");
+    }
+}
