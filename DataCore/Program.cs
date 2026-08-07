14e1eb6fe13fde18254d676a446835874e63560b
Jahel Corona Moreno <jcorona.0812@gmail.com>
2026-08-06 23:27:41 -0600

    feat: integrar flujo principal y reporte en consola

---
+++ DataCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using DataCore.Algorithms;
+using DataCore.Models;
+using DataCore.Presentation;
+using DataCore.Services;
+
+namespace DataCore;
+
+internal class Program
+{
+    static void Main()
+    {
+        RegistroDatos[] registros =
+    GeneradorRegistros.Crear(40);
+
+ReporteConsola.MostrarRegistros(
+    "ANTES DEL ORDENAMIENTO",
+    registros);
+
+MetricasOrdenacion metricas =
+    SelectionSorter.OrdenarPorSeleccion(registros);
+
+ReporteConsola.MostrarRegistros(
+    "DESPUÉS DEL ORDENAMIENTO",
+    registros);
+
+ReporteConsola.MostrarMetricas(metricas);
+    }
+}
